08bfa0d0977d8cc9f70f4eb426c12915ddf92d6d
tbaums <michael.a.tanenbaum@gmail.com>
2019-07-09 11:36:14 -0400

    working kafka producer api and client

--- kudo-kafka-app/kafka-client-api.py
+++ kudo-kafka-app/kafka-client-api.py
@@ -15,11 +15,18 @@ from flask import (
     stream_with_context,
     json,
 )
-# from kafka import (KafkaConsumer, KafkaProducer)
+from kafka import (
+    KafkaConsumer, 
+    KafkaProducer
+)
+
+if sys.version_info[0] == 3:
+    os.environ['PYTHONUNBUFFERED'] = '1'
 
 
+
+logging.basicConfig(level=logging.INFO)
 app = Flask(__name__)
-# logging.basicConfig(level=logging.INFO)
 
 
 ###################################
@@ -32,8 +39,8 @@ app = Flask(__name__)
 
 
 # WRITE
-# write (topic: <string>, payload: <JSON object>, repititions: <int>)
-# The `repititions` parameter indicates the number of times you want the message written to Kafka.
+# write (topic: <string>, payload: <JSON object>, repetitions: <int>)
+# The `repetitions` parameter indicates the number of times you want the message written to Kafka.
 # This allows simulation of high load write behavior to Kafka.
 
 
@@ -44,15 +51,43 @@ app = Flask(__name__)
 @app.route("/kafka-client-api/write", methods=['POST'])
 def write():
     query_string = urllib.parse.parse_qs(request.query_string.decode('UTF-8'))
-    sys.stdout.write("Request query_string: " + str(query_string))
-
-    return Response("MESSAGE SENT", mimetype="text/plain")
+    app.logger.info("Request query_string: " + str(query_string))
+    topic = query_string['topic'][0]
+    payload = query_string['payload'][0]
+    repetitions = int(query_string['repetitions'][0])
+    
+    write_to_kafka(topic, payload, repetitions)
+    return Response("WRITE REQUEST RECEIVED", mimetype='text/plain')
 
 
 ###################################
 #      KAFKA PRODUCER CLIENT      #
 ###################################
+def get_producer():
+    try:
+        producer_client = KafkaProducer(
+                                bootstrap_servers=[os.environ['BROKER_SERVICE']],
+                                value_serializer=lambda x: json.dumps(x).encode('utf-8'),
+                                acks=1,
 
+                                )
+    except Exception as e:
+        app.logger.error('Could not create producer_client with error ' + str(e))
+    return producer_client
+
+def write_to_kafka(topic, payload, repetitions):
+    producer_client = get_producer()
+    for _ in range(repetitions - 1):
+        try:
+            producer_client.send(topic, payload)
+            # producer_client.send(topic, value="test")
+            app.logger.info("Sent to Kafka. Topic: " + topic + " Value: " + str(payload))
+
+        except Exception as e:
+            app.logger.error("could not send message to " + topic + " with payload " + str(payload))
+            app.logger.error("send message failed with error: " + str(e))
+    producer_client.close()
+    return 
 
 ###################################
 #      KAFKA CONSUMER CLIENT      #
