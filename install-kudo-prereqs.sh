620ff856c827e8245a00dfee633b7fc7dc47e2ca
tbaums <michael.a.tanenbaum@gmail.com>
2019-07-18 09:33:33 -0400

    adding kafka monitoring yaml by default

--- install-kudo-prereqs.sh
+++ install-kudo-prereqs.sh
@@ -7,3 +7,6 @@ sleep 1
 
 kubectl create -f https://raw.githubusercontent.com/kudobuilder/kudo/master/docs/deployment/20-deployment.yaml
 
+sleep 1
+
+kubectl create -f https://raw.githubusercontent.com/kudobuilder/operators/master/repository/kafka/docs/v0.1/resources/service-monitor.yaml
